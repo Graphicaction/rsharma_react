391b18a9a68d62644456a9637fe66146411306e2
Rashmi Sharma <rksharma2679@gmail.com>
2020-05-13 10:29:56 -0400

    Adding routing and components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Wrapper from './components/Wrapper';
-import Portfolio from './pages/Portfolio';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import Row from "./components/Row";
+import Col from "./components/Col"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Wrapper>
-          <Portfolio />
-      </Wrapper>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Row>
+          <Col size="4">
+            <Navbar />
+          </Col>
+          <Col size ="8">  
+            <Wrapper>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/portfolio" component={Portfolio} />
+              <Route exact path="/contact" component={Contact} />
+            </Wrapper>
+          </Col>
+        </Row>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,35 +1,49 @@
-import React, { useEffect, useState } from "react";
-import API from "../utils/API";
-import ProjectContext from "../utils/ProjectContext";
-import Home from "../components/Home";
-import Row from "../components/Row";
+import React from "react";
 
-function Portfolio() {
-  const [projects, setProjects] = useState([]);
+const Portfolio = () => (
+  <div>
+    <h1>Portfolio Page</h1>
+    <p>
+      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
+      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
+      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
+      diam, sit amet facilisis lectus blandit at.
+    </p>
+  </div>
+);
+
+// import React, { useEffect, useState } from "react";
+// import API from "../utils/API";
+// import ProjectContext from "../utils/ProjectContext";
+// import Row from "../components/Row";
+
+// function Portfolio() {
+//   const [projects, setProjects] = useState([]);
   
-  // When the component mounts, a call will be made to get random users.
-  useEffect(() => {
-    loadProjects();
-  }, []);
+//   // When the component mounts, a call will be made to get random users.
+//   useEffect(() => {
+//     loadProjects();
+//   }, []);
 
-  function loadProjects() {
-    API.getProjects()
-      .then(projects => {
-        setProjects(projects);
-      })
-      .catch(err => console.log(err));
-  }
+//   function loadProjects() {
+//     API.getProjects()
+//       .then(projects => {
+//         setProjects(projects);
+//       })
+//       .catch(err => console.log(err));
+//   }
 
-  return (
-    <ProjectContext.Provider value={{ projects }}>
-      <div>
-        <h1 className="text-center">My latest Projects</h1>
-        <Row>
-          <Home />
-        </Row>
-      </div>
-    </ProjectContext.Provider>
-  );
-}
+//   return (
+//     <ProjectContext.Provider value={{ projects }}>
+//       <div>
+//         <h1 className="text-center">My latest Projects</h1>
+//         <Row>
+//           <div>Welcome to react portfolio!</div> 
+//         </Row>
+//       </div>
+//     </ProjectContext.Provider>
+//   );
+// }
 
 export default Portfolio;
