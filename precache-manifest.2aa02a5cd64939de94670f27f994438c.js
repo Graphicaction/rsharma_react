89321d0cb66cc8d570c2ccc5d76d84f88c672ca9
Rashmi Sharma <rksharma2679@gmail.com>
2020-05-21 21:11:14 -0400

    Updates

---
+++ precache-manifest.2aa02a5cd64939de94670f27f994438c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7743e975ca3c51ad15e7ea620a1acab0",
+    "url": "/rsharma_react/index.html"
+  },
+  {
+    "revision": "6f96779de2a0fc624209",
+    "url": "/rsharma_react/static/css/main.d0cf6c2a.chunk.css"
+  },
+  {
+    "revision": "710ca65bdc9f1bbde0b7",
+    "url": "/rsharma_react/static/js/2.b331286d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rsharma_react/static/js/2.b331286d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f96779de2a0fc624209",
+    "url": "/rsharma_react/static/js/main.dbaf60c2.chunk.js"
+  },
+  {
+    "revision": "255ca1dbc685e4cdfe35",
+    "url": "/rsharma_react/static/js/runtime-main.24702bdc.js"
+  },
+  {
+    "revision": "84fa92abdff9e8235d480f40efe0956f",
+    "url": "/rsharma_react/static/media/about.84fa92ab.png"
+  },
+  {
+    "revision": "944616fb59ea4c44be837e52ff696dd4",
+    "url": "/rsharma_react/static/media/backgroundImg.944616fb.jpg"
+  },
+  {
+    "revision": "927bbc62f78da11b8e355241bc3fe088",
+    "url": "/rsharma_react/static/media/burgerScreen.927bbc62.png"
+  },
+  {
+    "revision": "d302803cd70bd12993254e8dbd9e623a",
+    "url": "/rsharma_react/static/media/code-query.d302803c.png"
+  },
+  {
+    "revision": "5663a379a3036cc24e32e8aa686b37cf",
+    "url": "/rsharma_react/static/media/employeeDirScreen.5663a379.png"
+  },
+  {
+    "revision": "0b477f8ac654261c0131952e38b4866f",
+    "url": "/rsharma_react/static/media/employeemgnt.0b477f8a.gif"
+  },
+  {
+    "revision": "e8332c7911c3cdaf265d3844deb1931e",
+    "url": "/rsharma_react/static/media/mypic.e8332c79.jpg"
+  },
+  {
+    "revision": "ce97f090bbaac65e55b665d26178d0ce",
+    "url": "/rsharma_react/static/media/readScreen.ce97f090.gif"
+  },
+  {
+    "revision": "04d2a5f89f7a0143a34a8387194e250e",
+    "url": "/rsharma_react/static/media/weatherScreen.04d2a5f8.png"
+  },
+  {
+    "revision": "4b24dc27707f21da17dad0e0e1a92bc3",
+    "url": "/rsharma_react/static/media/workoutScreen.4b24dc27.png"
+  }
+]);
