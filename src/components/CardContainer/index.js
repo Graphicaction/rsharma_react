6b0a4e4c569ba52bacaea02f35481dfc94e5e5f3
Rashmi Sharma <rksharma2679@gmail.com>
2020-06-11 17:05:58 -0400

    adjusting background image

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -7,12 +7,12 @@ function CardContainer() {
   //Assigning projects 
   const { projects } = useContext(ProjectContext);
     return(
-      <div className="">
+      <div>
         {/* Displaying all projects in card's format*/}
         <div className="row d-flex justify-content-center">
           {projects.map(project => (
           <div className="col-sm-5 col-md-5 col-lg-5 my-4" key={project.id}>
-              <div className="card border-0 text-center">
+              <div className="card border-dark text-center">
                 <a rel="noopener noreferrer" href={project.deployLink} target="_blank">
                     <img src={require(`../../images/${project.image}`)} className="card-img" alt="budget-tracker" key={project.name} /></a>
                 <div className="card-body">
